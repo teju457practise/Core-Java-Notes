d4fb0d8597bf1852759013e3affdf14bcd052acd
tejusmg457 <tejasrhonnali457@gmail.com>
2023-05-17 13:28:38 +0530

    First commit

---
+++ src/C.java
@@ -0,0 +1,34 @@
+class a extends Thread {
+
+	public void run() {
+		
+		for(int i = 0 ; i <100 ; i++) {
+			System.out.println("hello");
+		}
+		
+	}
+}
+
+class b extends Thread {
+	
+	public void run() {
+		
+		for(int i = 0 ; i <100 ; i++) {
+			System.out.println("hii");
+		}
+		
+	}
+}
+
+public class C {
+
+	public static void main(String[] args) {
+	
+		a obj = new a();
+		b obj1 = new b();
+		
+		obj.start();
+		obj1.start();
+	}
+
+}
